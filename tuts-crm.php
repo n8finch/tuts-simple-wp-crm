e4bacaa2ab019680492b0dc52634912676cc2750
Nate Finch <n8finch@Megan-Finchs-MacBook.local>
2015-05-08 16:02:47 +0900

    added tuts-crm.php

---
+++ tuts-crm.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Tuts+ CRM
+ * Plugin URI: #
+ * Version: 1.0
+ * Author: Tuts+
+ * Author URI: http://code.tutsplus.com
+ * Description: A simple CRM system for WordPress
+ * License: GPL2
+ */
+?>
+
